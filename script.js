616c2270e04c0781b8cf3edadc1d4cc1bafebac7
Carlomadella <madella871@gmail.com>
2025-03-17 17:54:20 +0100

    create prime variabili

---
+++ script.js
@@ -0,0 +1,9 @@
+let journeyLenght = parseInt(prompt("Indica i kilometri che vuoi percorrere con questo treno?"));
+let passengerAge = parseInt(prompt("Indica l'età del passengero"));
+let priceKm = 0.21;
+
+console.log(journeyLenght)
+console.log(passengerAge)
+
+
+
