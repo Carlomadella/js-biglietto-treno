becef246e1b81ef29064d91925bb98a25b4fa362
Carlomadella <madella871@gmail.com>
2025-03-17 18:34:03 +0100

    creato else if

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ let journeyLenght = parseInt(prompt("Indica i kilometri che vuoi percorrere con 
 let passengerAge = parseInt(prompt("Indica l'età del passengero"));
 let priceKm = 0.21;
 
-console.log(journeyLenght)
-console.log(passengerAge)
+// console.log(journeyLenght)
+// console.log(passengerAge)
 
 let prezzoIntero = (parseInt(priceKm * journeyLenght));
 
-console.log("Il prezzo finale è di", prezzoIntero, "€");
+// console.log("Il prezzo finale è di", prezzoIntero, "€");
 
 let prezzoUnder;
+let prezzoOver;
 
 if (passengerAge<18) {
     prezzoUnder = (prezzoIntero-( 20/100 ))
     console.log("Il prezzo del biglietto è scontato del 20%, perciò il totale sarà di", prezzoUnder, "€")
 } 
+else if (passengerAge>65){
+    prezzoOver = (prezzoIntero-( 40/100 ))
+    console.log("Il prezzo del biglietto è scontato del 40%, perciò il totale sarà di", prezzoOver, "€")
+}
 
 
